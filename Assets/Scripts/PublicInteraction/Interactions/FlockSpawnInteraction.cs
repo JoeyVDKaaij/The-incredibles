30844848887e264eef66fe27f2961a2444476626
Codrin Ilie <125357154+CodyI20@users.noreply.github.com>
2024-06-12 21:35:12 +0200

    Bug fixes
    - Fixed the "duck" mechanic": Now making it possible while walking in real life.
    - WIP: Flock mechanic. The flock doesn't behave as intended yet.

--- Assets/Scripts/PublicInteraction/Interactions/FlockSpawnInteraction.cs
+++ Assets/Scripts/PublicInteraction/Interactions/FlockSpawnInteraction.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FlockSpawnInteraction", menuName = "Interactions/FlockSpawnInteraction")]
 public class FlockSpawnInteraction : InteractionBehavior
 {
-    [SerializeField] private Flock flockPrefab;
+    [SerializeField] private GameObject flockPrefab;
     [SerializeField] private float timeAlive = 5f;
     [SerializeField] private Vector3 offSet = new Vector3(0, 3, 0);
     protected override void DoAction()
     {
         Debug.Log($"Spawning Flock");
-        Flock flock = Instantiate(flockPrefab, player.position + offSet, Quaternion.identity);
-        Destroy(flock.gameObject, timeAlive);
+        GameObject flock = Instantiate(flockPrefab, player.position + offSet, Quaternion.identity);
+        Destroy(flock, timeAlive);
     }
 }
