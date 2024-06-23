d0f7f6f047179815deab51dbdd8453324d5b1130
Codrin Ilie <125357154+CodyI20@users.noreply.github.com>
2024-06-23 11:26:58 +0200

    Tests + Implementations
    - Tested the heavy object on bridge to wobble for implementation
    - Added the rope with bones model and implemented it into the test scene. Tested a few parameters with it and tweaked some values to make it look as good as possible
    - Created a new interaction with the bow where if you drop it, it will despawn after a certain amount of time. When you reach into your quiver without the bow in your other hand, it will spawn the bow.

--- Assets/Scripts/Bow/Quiver.cs
+++ Assets/Scripts/Bow/Quiver.cs
@@ -4,21 +4,48 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Quiver : XRBaseInteractable
 {
     [SerializeField] private GameObject arrowPrefab = null;
+    [SerializeField] private GameObject bowPrefab = null;
+    private bool bowSpawned = false;
 
     protected override void OnEnable()
     {
         base.OnEnable();
         selectEntered.AddListener(CreateAndSelectArrow);
+        selectEntered.AddListener(CreateAndSelectBow);
+        DespawnBow.OnBowDespawned += SetBowSpawnedFalse;
     }
 
     protected override void OnDisable()
     {
         base.OnDisable();
         selectEntered.RemoveListener(CreateAndSelectArrow);
+        selectEntered.RemoveListener(CreateAndSelectBow);
+        DespawnBow.OnBowDespawned -= SetBowSpawnedFalse;
+    }
+
+    private void SetBowSpawnedFalse()
+    {
+        bowSpawned = false;
+    }
+
+    private void CreateAndSelectBow(SelectEnterEventArgs args)
+    {
+        // Create bow, force into interacting hand
+        if(bowSpawned)
+        {
+            return;
+        }
+        Bow bow = Instantiate(bowPrefab, args.interactorObject.transform.position, args.interactorObject.transform.rotation).GetComponent<Bow>();
+        interactionManager.SelectEnter(args.interactorObject, bow);
+        bowSpawned = true;
     }
 
     private void CreateAndSelectArrow(SelectEnterEventArgs args)
     {
+        if(!bowSpawned)
+        {
+            return;
+        }
         // Create arrow, force into interacting hand
         Arrow arrow = CreateArrow(args.interactorObject.transform);
         interactionManager.SelectEnter(args.interactorObject, arrow);
