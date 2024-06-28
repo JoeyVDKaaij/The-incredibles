5dc366a20e741b8197f7e68a3472eed8aafd4e23
Codrin Ilie <125357154+CodyI20@users.noreply.github.com>
2024-06-28 12:27:45 +0200

    MenuUpdate

--- Assets/Scripts/MenuSettingsScript.cs
+++ Assets/Scripts/MenuSettingsScript.cs
@@ -5,7 +5,7 @@ public class MenuSettingsScript : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void RestartScene()
